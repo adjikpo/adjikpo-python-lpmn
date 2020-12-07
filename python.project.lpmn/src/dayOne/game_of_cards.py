cbec89b82a983932c9cc78c20eb8cf2ea60a2c6b
adjikpo <arthur.djikpo@gmail.com>
2020-12-08 00:13:04 +0100

    add the last exercises of DayOne

---
+++ python.project.lpmn/src/dayOne/game_of_cards.py
@@ -0,0 +1,7 @@
+#coding:utf-8
+
+gameOfCards=["1","2","3","4","5","6","7","8","9","10","V","D","R"]
+for card in gameOfCards:
+    print(card)
+
+print (gameOfCards)
---
+++ python.project.lpmn/src/dayOne/list_string.py
@@ -0,0 +1,6 @@
+#coding:utf-8
+
+print("Hello everyone !")
+stringWord = str(input("Please write your word: \n"))
+
+print (len(stringWord))
