3200d1121451c0b62d5ec575ac24a2eda791ed2f
adjikpo <arthur.djikpo@gmail.com>
2020-12-09 22:16:37 +0100

    add the dayThree scprits (bdd tp)

---
+++ python.project.lpmn/src/dayThree/bddTp/data/moduleBdd.py
@@ -0,0 +1,334 @@
+from sqlite3 import *
+
+#Connecting to sqlite
+connection = connect("base.db")
+
+#Creating a cursor object using the cursor() method
+cursor = connection.cursor()
+
+#Doping ALL table if already exists.
+cursor.execute("DROP TABLE IF EXISTS STUDENT")
+cursor.execute("DROP TABLE IF EXISTS CURRICULUM")
+cursor.execute("DROP TABLE IF EXISTS LANGUAGE")
+cursor.execute("DROP TABLE IF EXISTS CURRICULUM_LANGUAGE")
+
+print("Database was cleaned successfully........")
+
+####################### CREATE ALL TABLE ####################################
+
+# Create table - STUDENT
+sql ='''CREATE TABLE STUDENT(
+   idStudent INTEGER AUTO_INCREMENT PRIMARY KEY, 
+   firstName TEXT NOT NULL, 
+   lastName TEXT NOT NULL, 
+   age INTEGER NOT NULL,
+   idCurriculum INTEGER DEFAULT NULL,
+   CONSTRAINT fk_id_curriculum FOREIGN KEY (idCurriculum) REFERENCES CURRICULUM(idCurriculum)
+)'''
+cursor.execute(sql)
+
+# Commit your changes in the database
+connection.commit()
+print("Table STUDENT created successfully........")
+
+# Create table - CURRICULUM
+sql ='''CREATE TABLE CURRICULUM(
+   idCurriculum INTEGER AUTO_INCREMENT PRIMARY KEY, 
+   nameCurriculum TEXT NOT NULL
+)'''
+cursor.execute(sql)
+
+# Commit your changes in the database
+connection.commit()
+print("Table CURRICULUM created successfully........")
+
+# Create table - LANGUAGE
+sql ='''CREATE TABLE LANGUAGE(
+   idLanguage INTEGER AUTO_INCREMENT PRIMARY KEY, 
+   nameLanguage TEXT NOT NULL
+)'''
+cursor.execute(sql)
+
+# Commit your changes in the database
+connection.commit()
+print("Table LANGUAGE created successfully........")
+
+# Create table - CURRICULUM_LANGUAGE
+sql ='''CREATE TABLE CURRICULUM_LANGUAGE(
+   idCurriculum INTEGER DEFAULT NULL,
+   idLanguage INTEGER DEFAULT NULL,
+   FOREIGN KEY (idCurriculum) REFERENCES CURRICULUM(idCurriculum),
+   FOREIGN KEY (idLanguage) REFERENCES LANGUAGE(idLanguage)
+   UNIQUE (idCurriculum, idLanguage)
+
+)'''
+cursor.execute(sql)
+
+# Commit your changes in the database
+connection.commit()
+print("Table CURRICULUM_LANGUAGE created successfully........")
+
+
+######################### ADD FUNCTIONS #####################################
+
+def addStudent(firstname, lastname, age, curriculum):
+   #Insert into table - STUDENT
+   curriculumId = getIdCurriculum(curriculum)
+   student = (cursor.lastrowid, firstname, lastname, age, curriculumId)
+   cursor.execute('INSERT INTO STUDENT VALUES(?, ?, ?, ?, ?)', student)
+   # Commit your changes in the database
+   connection.commit()
+   print("Records inserted........")
+
+
+def addCurriculum(nameCurriculum):
+   #Insert into table - CURRICULUM
+   curriculum = (cursor.lastrowid, nameCurriculum)
+   cursor.execute('INSERT INTO CURRICULUM VALUES(?, ?)', curriculum)
+   # Commit your changes in the database
+   connection.commit()
+   print("Records inserted........")
+
+def addLanguage(nameLanguage):
+   #Insert into table - LANGUAGE
+   language = (cursor.lastrowid, nameLanguage)
+   cursor.execute('INSERT INTO LANGUAGE VALUES(?, ?)', language)
+   # Commit your changes in the database
+   connection.commit()
+   print("Records inserted........")
+
+def addLanguageToCurriculum(nameLanguage, nameCurriculum):
+   #Insert into table - LANGUAGE
+   language = getIdLanguage(nameLanguage)
+   curriculum = getIdCurriculum(nameCurriculum)
+   curriculum_language = (curriculum, language)
+
+   cursor.execute('INSERT INTO CURRICULUM_LANGUAGE VALUES(?, ?)', curriculum_language)
+   # Commit your changes in the database
+   connection.commit()
+   print("Records inserted........")
+
+####################### GET FUNCTIONS #######################################
+
+def getAllStudents():
+   cursor.execute('''SELECT * FROM STUDENT''')
+
+   #Fetching all row from the table
+   allStudents = cursor.fetchall()
+   allStudents = filter(None, allStudents)
+   for i in allStudents:
+      print(i[4] + ' : ' + i[1] + ' ' + i[2] + ' ' + str(i[3]) + 'yrs')   
+
+def getOneStudent(name,age):
+   #Retrieving data
+   student=(name,age)
+   cursor.execute('''SELECT * FROM STUDENT WHERE firstname=? AND age=?''',student)
+
+   #Fetching a row from the table
+   print(cursor.fetchone())
+
+def getAllCurriculums():
+   cursor.execute('''SELECT * FROM CURRICULUM ''')
+
+   #Fetching all row from the table
+   allCurriculums = cursor.fetchall()
+   for i in allCurriculums:
+      print(i[1]) 
+
+def getOneCurriculum(name):
+   #Retrieving data
+   curriculum=(name,)
+   cursor.execute('''SELECT * FROM CURRICULUM WHERE nameCurriculum=? ''',curriculum)
+
+   #Fetching a row from the table
+   print(cursor.fetchone())
+
+def getAllLanguages():
+   cursor.execute('''SELECT * FROM LANGUAGE ''')
+
+   #Fetching all row from the table
+   allLanguages = cursor.fetchall()
+   for i in allLanguages:
+      print(i[1]) 
+
+def getOneLanguage(name):
+   #Retrieving data
+   language=(name,)
+   cursor.execute('''SELECT * FROM LANGUAGE WHERE nameLanguage=? ''',language)
+
+   #Fetching a row from the table
+   print(cursor.fetchone())
+
+def getOneLanguageById(id):
+   #Retrieving data
+   language=(id,)
+   cursor.execute('''SELECT * FROM LANGUAGE WHERE idLanguage=? ''',language)
+
+   #Fetching a row from the table
+   print(cursor.fetchone())
+
+def getIdLanguage(name):
+   language=(name,)
+   cursor.execute('''SELECT idLanguage FROM LANGUAGE WHERE nameLanguage=? ''',language)
+
+   #Fetching a row from the table
+   idLanguage = cursor.fetchone()
+   return idLanguage[0]
+
+def getIdCurriculum(name):
+   curriculum=(name,)
+   cursor.execute('''SELECT idCurriculum FROM CURRICULUM WHERE nameCurriculum=? ''',curriculum)
+
+   #Fetching a row from the table
+   idCurriculum = cursor.fetchone()
+   return idCurriculum[0]
+
+def getIdStudent(firstname, lastname):
+   student=(firstname,lastname)
+   cursor.execute('''SELECT idStudent FROM STUDENT WHERE firstname=? AND lastname=? ''',student)
+
+   #Fetching a row from the table
+   idStudent = cursor.fetchone()
+   return idStudent[0]
+
+####################### UPDATE FUNCTION #######################################
+
+def updateStudent(firstname, lastname, age, idStudent):
+   update_student = (firstname, lastname, age, idStudent)
+   cursor.execute('''UPDATE STUDENT SET firstName=?, lastName=?, age=? WHERE idStudent=?''', update_student)
+   # Commit your changes in the database
+   connection.commit()
+   print("Table STUDENT updated...... ")
+
+def updateCurriculum(nameCurricullum,idCurriculum):
+   update_curriculum = (nameCurricullum, idCurriculum)
+   cursor.execute('''UPDATE CURRICULUM SET nameCurriculum=? WHERE idCurriculum=?''', update_curriculum)
+   # Commit your changes in the database
+   connection.commit()
+   print("Table CURRICULUM updated...... ")
+
+def updateLanguage(nameLanguage,idLanguage):
+   update_language = (nameLanguage, idLanguage)
+   cursor.execute('''UPDATE LANGUAGE SET nameLanguage WHERE idLanguage=?''', update_language)
+   # Commit your changes in the database
+   connection.commit()
+   print("Table LANGUAGE updated...... ")
+
+
+####################### DELETE FUNCTION #######################################
+
+def deleteOneStudent(firstname, lastname):
+   idStudent = getIdStudent(firstname,lastname)
+   delete_student = (idStudent,)
+
+   #Deleting records
+   cursor.execute('''DELETE FROM STUDENT WHERE idStudent=?''', delete_student)
+
+   # Commit your changes in the database
+   connection.commit()
+   print("Table updated after delete...... ")
+
+def deleteOneCurriculum(name):
+   idCurriculum = getIdCurriculum(name)
+   delete_curriculum = (idCurriculum,)
+   #Deleting records
+   cursor.execute('''DELETE FROM CURRICULUM WHERE idCurriculum=?''',delete_curriculum)
+
+   # Commit your changes in the database
+   connection.commit()
+   print("Table updated after delete...... ")
+
+def deleteOneLanguage(name):
+   idLanguage = getIdLanguage(name)
+   delete_language = (idLanguage)
+   #Deleting records
+   cursor.execute('''DELETE FROM LANGUAGE WHERE idLanguage=?''', delete_language)
+
+   # Commit your changes in the database
+   connection.commit()
+   print("Table updated after delete...... ")
+
+
+######################## ANOTHER FUNCTIONS ######################################
+
+def getStudentsOneCurriculum(name):
+   studentsOneCurriculum = (name,)
+   cursor.execute('''SELECT * FROM CURRICULUM WHERE nameCurriculum=?''',studentsOneCurriculum)
+
+   #Fetching all row from the table
+   print(cursor.fetchall())
+
+def getLanguagesOneCurriculum(nameCurriculum):
+   nameCurriculum = (nameCurriculum,)
+   cursor.execute('''SELECT * FROM CURRICULUM WHERE nameCurriculum=?''',nameCurriculum)
+
+   languagesId = cursor.fetchall()
+   languaguesName = []
+   for i in languagesId :
+      languaguesName.append[getOneLanguageById(i)]
+
+   print('All languages on {} curriculum: \n'.format(nameCurriculum))
+   print(languaguesName)
+
+####################### FIXTURES #######################################
+
+def installFixtures():
+
+   new_student1 = (cursor.lastrowid, "Arthur", "Djikpo", 24,"Devops")
+   cursor.execute('INSERT INTO STUDENT VALUES(?, ?, ?, ?, ?)', new_student1)
+
+   new_student2 = (cursor.lastrowid, "Ettiènne", "Thunder", 69, "Devops")
+   cursor.execute('INSERT INTO STUDENT VALUES(?, ?, ?, ?, ?)', new_student2)
+
+   new_student3 = (cursor.lastrowid, "azerty", "test", 24, "Dev Mobile")
+   cursor.execute('INSERT INTO STUDENT VALUES(?, ?, ?, ?, ?)', new_student3)
+
+   new_student4 = (cursor.lastrowid, "qsdfg", "test2", 69, "Dev Python")
+   cursor.execute('INSERT INTO STUDENT VALUES(?, ?, ?, ?, ?)', new_student4)
+
+   print('Students fixtures : ok ...... ')
+
+   curriculum1 = (cursor.lastrowid, "Devops")
+   cursor.execute('INSERT INTO CURRICULUM VALUES(?, ?)', curriculum1)
+
+   curriculum2 = (cursor.lastrowid, "Dev Mobile")
+   cursor.execute('INSERT INTO CURRICULUM VALUES(?, ?)', curriculum2)
+
+   curriculum3 = (cursor.lastrowid, "Dev Python")
+   cursor.execute('INSERT INTO CURRICULUM VALUES(?, ?)', curriculum3)
+
+   print('Curriculum fixtures : ok ...... ')
+
+   language1 = (cursor.lastrowid, "Python")
+   cursor.execute('INSERT INTO LANGUAGE VALUES(?, ?)', language1)
+
+   language2 = (cursor.lastrowid, "Java")
+   cursor.execute('INSERT INTO LANGUAGE VALUES(?, ?)', language2)
+
+   language3 = (cursor.lastrowid, "Shell")
+   cursor.execute('INSERT INTO LANGUAGE VALUES(?, ?)', language3)
+
+
+   print('Language fixtures : ok ...... ')
+
+
+   language1 = getIdLanguage("Python")
+   curriculum1 = getIdCurriculum("Devops")
+   curriculum_language1 = (curriculum1, language1)
+
+   cursor.execute('INSERT INTO CURRICULUM_LANGUAGE VALUES(?, ?)', curriculum_language1)
+
+   language2 = getIdLanguage("Java")
+   curriculum2 = getIdCurriculum("Dev Mobile")
+   curriculum_language2 = (curriculum2, language2)
+
+   cursor.execute('INSERT INTO CURRICULUM_LANGUAGE VALUES(?, ?)', curriculum_language2)
+
+   language3 = getIdLanguage("Java")
+   curriculum3 = getIdCurriculum("Devops")
+   curriculum_language3 = (curriculum3, language3)
+
+   cursor.execute('INSERT INTO CURRICULUM_LANGUAGE VALUES(?, ?)', curriculum_language3)
+
+   print('Curiculum & Language fixtures : ok ...... ')
+
---
+++ python.project.lpmn/src/dayThree/bddTp/main/main.py
@@ -0,0 +1,300 @@
+#coding:utf-8
+
+import sys
+import os
+import time
+
+os.system('clear')
+
+sys.path.append('..')
+from data.moduleBdd import *
+
+# Wait for 5 seconds
+time.sleep(1)
+os.system('clear')
+
+def get_menu_choice():
+    def print_menu():       # Your menu design here
+        print(30 * "-", "TRAINING MANAGEMENT", 30 * "-")
+        print("01. Add a student ")
+        print("02. Add a curriculum ")
+        print("03. Add a language ")
+        print("04. Add a language in curriculum ")
+        print("05. Show all student")
+        print("06. Show all curriculum")
+        print("07. Show all language")  
+        print("08. Update a student")
+        print("09. Update a curriculum")
+        print("10. Update a language")
+        print("11. Delete a student")
+        print("12. Delete a curriculum")
+        print("13. Delete a language")
+        print("14. Show all students on a curriculum")
+        print("15. Show all languages on a curriculum")
+        print("16. Show currilum for a languages")
+        print("-1. Install and test fixtures")      
+        print("0. Exit  ")
+        print(81 * "-")
+
+    loop = True
+    
+
+    while loop:          # While loop which will keep going until loop = False
+        print_menu()    # Displays menu
+        choice = input("Enter your choice [1-16]: ")
+
+        if choice == '1':
+            choice = ''
+            while len(choice) == 0:
+                os.system('clear')
+                print('01. Add a student')
+                try:
+                    firstName = str(input("Enter a firstname : \n"))
+                    lastName = str(input("Enter a lastname : \n"))
+                    age = int(input("Enter a age : \n"))
+
+                    # print("List of Curriculum:\n{}".format(getAllCurriculums()))
+                    print("List of Curriculum:\n")
+                    print(getAllCurriculums())  
+                    curriculum = str(input("Choose a curriculum:\n"))
+
+                    if len(firstName) == 0:
+                        print("Please enter your fistname.\n")
+                        # Wait for 3 seconds
+                        time.sleep(3)
+                        if len(lastName) == 0:
+                            print("Please enter your lastname.\n")
+                            # Wait for 3 seconds
+                            time.sleep(3)
+                            break
+                        break
+                    
+                    addStudent(firstName,lastName,age,curriculum)
+                except ValueError:
+                    print("Records not inserted........\nPlease enter a correct value for the age\n")
+                    # Wait for 3 seconds
+                    time.sleep(3)
+
+                os.system('clear')
+                break
+            loop = True
+        elif choice == '2':
+            choice = ''
+            while len(choice) == 0:
+                os.system('clear')
+                print('02. Add a curriculum')
+
+                curriculum = str(input("Enter a curriculum : "))
+                if len(curriculum) == 0:
+                    print("Please enter a curriculum.\n")
+                    # Wait for 3 seconds
+                    time.sleep(3)
+                    break
+
+                addCurriculum(curriculum)
+
+                os.system('clear')
+                break
+            
+            loop = True
+        elif choice == '3':
+            choice = ''
+            while len(choice) == 0:
+                os.system('clear')
+                print('Add a language')
+
+                language = str(input("Enter a language : "))
+                if len(language) == 0:
+                    print("Please enter a language.\n")
+                    # Wait for 3 seconds
+                    time.sleep(3)
+                    break
+                
+                addLanguage(language)
+                
+                os.system('clear')
+                break
+            
+            loop = True
+        elif choice == '4':
+            choice = ''
+            while len(choice) == 0:
+                os.system('clear')
+                print('Add a language in curriculum')
+                
+                curriculum = str(input("Enter a curriculum : "))
+                language = str(input("Enter a language : "))
+
+                print(addLanguageToCurriculum(curriculum,language))
+
+                os.system('clear')
+                break
+            
+            loop = True
+        elif choice == '5':
+            choice = ''
+            while len(choice) == 0:
+                os.system('clear')
+                print('05. Show all students')
+                
+                getAllStudents()
+
+                break
+            loop = True
+        elif choice == '6':
+            choice = ''
+            while len(choice) == 0:
+                os.system('clear')
+
+                getAllCurriculums()
+
+                
+                break
+            
+            loop = True
+        elif choice == '7':
+            choice = ''
+            while len(choice) == 0:
+                os.system('clear')
+
+                getAllLanguages()
+
+                
+                break
+            
+            loop = True
+        elif choice == '8':
+            choice = ''
+            while len(choice) == 0:
+                os.system('clear')
+
+                try:
+                    print(getAllStudents())
+                    oldFirstname = str(input("Enter the old fistname:\n"))
+                    oldLastname = str(input("Enter the old lastname:\n"))
+
+                    idStudent = getIdStudent(oldFirstname, oldLastname)
+
+                    firstName = str(input("Enter a new firstname :\n"))
+                    lastName = str(input("Enter a new lastname : \n"))
+                    age = int(input("Enter a age : \n"))
+
+                    print(updateStudent(firstName,lastName,age, idStudent))
+                except ValueError:
+                    print("Records not inserted........\nPlease enter a correct value for the age\n")
+                    # Wait for 3 seconds
+                    time.sleep(3)
+
+                os.system('clear')
+                break
+            
+            loop = True
+        elif choice == '9':
+            choice = ''
+            while len(choice) == 0:
+                os.system('clear')
+
+                oldCurriculum = str(input("Enter the old curriculum name :\n"))
+
+                idCurriculum = getIdCurriculum(oldCurriculum)
+
+                curriculum = str(input("Enter the new curriculum name : \n"))
+
+                print(updateCurriculum(curriculum, idCurriculum))
+
+                os.system('clear')
+                break
+            
+            loop = True
+        elif choice == '10':
+            choice = ''
+            while len(choice) == 0:
+                os.system('clear')
+
+                oldLanguage = str(input("Enter the old language name :\n"))
+
+                idLanguage = getIdLanguage(oldLanguage)
+
+                language = str(input("Enter the new languages name : \n"))
+
+                print(updateLanguage(language, idLanguage))
+                
+                os.system('clear')
+                break
+            
+            loop = True
+        elif choice == '11':
+            choice = ''
+            while len(choice) == 0:
+                os.system('clear')
+                
+                print('List of students :')
+                getAllStudents()
+                
+                print('\nDeleting a student ....\n')
+                firstname = str(input("Enter the firstname:\n"))
+                lastname = str(input('Enter the lastname:\n'))
+                deleteOneStudent(firstname, lastname)
+
+                os.system('clear')
+                break
+            
+            loop = True  
+        elif choice == '12':
+            choice = ''
+            while len(choice) == 0:
+                os.system('clear')
+                
+                print('List of curriculums :')
+                getAllCurriculums()
+                
+                print('\nDeleting a curriculum ....\n')
+                curriculum = str(input('Enter the curriculum name:\n'))
+                deleteOneCurriculum(curriculum)
+
+                os.system('clear')
+                break
+            
+            loop = True  
+        elif choice == '13':
+            choice = ''
+            while len(choice) == 0:
+                os.system('clear')
+                
+                print('List of languages :')
+                getAllLanguages()
+                
+                print('\nDeleting a language ....\n')
+                try:
+                    language = str(input('Enter the language name:\n'))
+                    deleteOneLanguage(language)
+                except ValueError as e:
+                    print(e)
+                    # Wait for 1 seconds
+                    time.sleep(10)
+
+                os.system('clear')
+                break
+            
+            loop = True  
+        elif choice == '-1':
+            choice = ''
+            while len(choice) == 0:
+                os.system('clear')
+                installFixtures()
+                # Wait for 1 seconds
+                time.sleep(1)
+                os.system('clear')
+                break
+            
+            loop = True      
+        elif choice == '0':
+            print("Exiting..")
+            loop = False  # This will make the while loop to end
+        else:
+            # Any inputs other than values 1-16 we print an error message
+            input("Wrong menu selection. Enter any key to try again..")
+    return choice
+
+
+print(get_menu_choice())
