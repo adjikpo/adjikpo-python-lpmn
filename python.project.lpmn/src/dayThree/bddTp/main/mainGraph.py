3bef14b1e83cfd8880a22797d000d219748ab49a
adjikpo <arthur.djikpo@gmail.com>
2020-12-11 09:39:51 +0100

    add a main graph for the moduleBdd

---
+++ python.project.lpmn/src/dayThree/bddTp/main/mainGraph.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+
+from tkinter import *
+
+root = Tk()
+root.geometry("500x500")
+root.title("TRAINING MANAGEMENT")
+
+# frame 2
+Frame1 = Frame(root, text="TRAINING MANAGEMENT",borderwidth=2, relief=GROOVE)
+Frame1.pack(side=LEFT, padx=30, pady=30)
+
+# frame 2
+Frame2 = Frame(root, borderwidth=2, relief=GROOVE)
+Frame2.pack(side=LEFT, padx=10, pady=10)
+
+
+
+
+root.mainloop()
